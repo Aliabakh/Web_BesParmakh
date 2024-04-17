1824d4d7ea68e9503f2f0ebb5a583d90251bec68
Aliabakh <126652786+Aliabakh@users.noreply.github.com>
2024-04-17 16:06:33 +0800

    test push
    ali
    
    Co-Authored-By: Khamza Tilyek <tilekkhamza9702@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AppProvidersWrapper, BackToTop } from "./components";
 import AllRoutes from "./routes/Routes";
 import { configureFakeBackend } from "@/common";
 
-// styles
+// styles test
 import "@/assets/css/style.css";
 
 configureFakeBackend();
